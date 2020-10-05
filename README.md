7e3dd538927cae3eb7b134d847f5ebcdf2b61c3f
samirchaves <samirbraga@live.com>
2020-10-04 23:43:40 -0300

    Adicionando demais parâmetros

--- README.md
+++ README.md
@@ -8,4 +8,12 @@ A presente implementação traz uma tabela de dispersão usando Simple Tabulatio
 python main.py -i <input-file> -o <output-file>
 ```
 
-Também é possível informar o tamanho inicial da tabela com o parâmetro `-s`.
+### Parâmetros
+
+| Short | Long                | Valor   | Valor padrão | Descrição                                                                                                            |
+|-------|---------------------|---------|--------------|----------------------------------------------------------------------------------------------------------------------|
+| -i    | --input             | string  | input.txt    | Caminho do arquivo de comandos de entrada.                                                                           |
+| -o    | --output            | string  | output.txt   | Caminho do arquivo de logs de saída.                                                                                 |
+| -s    | --table-size        | integer | 4            | Tamanho inicial da tabela.                                                                                           |
+| -d    | --double-threshold  | float   | 0.75         | Valor de 0 a 1 representando a porcentagem de preenchimento na qual a tabela será dobrada.               |
+| -h    | --halving-threshold | float   | 0.25         | Valor de 0 a 1 representando a porcentagem de preenchimento na qual a tabela será diminuída pela metade. |
--- main.py
+++ main.py
@@ -7,16 +7,18 @@ from input_parser import parse_input
 argument_list = sys.argv[1:]
 
 # Options
-options = "i:o:s:"
+options = "i:o:s:d:h:"
 
 # Long options
-long_options = ["input", "output", "table-size"]
+long_options = ["input=", "output=", "table-size=", "doubling-threshold=", "halving-threshold="]
 
 
 def main():
     input_file = "./input.txt"
     output_file = "./output.txt"
     table_size = 4
+    doubling_threshold = 0.75
+    halving_threshold = 0.25
     try:
         arguments, values = getopt.getopt(argument_list, options, long_options)
         for currentArgument, currentValue in arguments:
@@ -26,12 +28,19 @@ def main():
                 output_file = currentValue
             elif currentArgument in ("-s", "--table-size"):
                 table_size = int(currentValue)
+            elif currentArgument in ("-d", "--doubling-threshold"):
+                doubling_threshold = float(currentValue)
+            elif currentArgument in ("-h", "--halving-threshold"):
+                halving_threshold = float(currentValue)
     except getopt.error as err:
         print(str(err))
 
     inputs = parse_input(input_file)
     logger = Logger()
-    hashtable = Hashtable(table_size, logger)
+    hashtable = Hashtable(table_size,
+                          logger,
+                          grow_threshold=doubling_threshold,
+                          shrink_threshold=halving_threshold)
 
     for ipt in inputs:
         cmd, value = ipt
